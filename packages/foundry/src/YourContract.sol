57e7c69da02b7d213aa96eb49d2e6b8661f9d167
NME.eth <maximus.holm@gmail.com>
2022-06-19 16:52:01 +0300

    replacing hardhat with foundry

--- packages/foundry/src/YourContract.sol
+++ packages/foundry/src/YourContract.sol
@@ -1,8 +1,8 @@
 pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
-import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol"; 
+import "forge-std/console.sol";
+// import "openzeppelin-contracts/contracts/access/Ownable.sol"; 
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract YourContract {
